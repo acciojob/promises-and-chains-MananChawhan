89775d3fc2d2a1c0eef64f80497174f324b8f0ed
MananChawhan <121217947+MananChawhan@users.noreply.github.com>
2025-06-02 10:14:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-//your JS code here. If required.
+document.getElementById("voteForm").addEventListener("submit", function (e) {
+  e.preventDefault(); 
+  const name = document.getElementById("name").value.trim();
+  const age = document.getElementById("age").value.trim();
+
+  // Validate input
+  if (name === "" || age === "") {
+    alert("Please enter valid details.");
+    return;
+  }
+
+  const checkEligibility = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (parseInt(age) >= 18) {
+        resolve(`Welcome, ${name}. You can vote.`);
+      } else {
+        reject(`Oh sorry ${name}. You aren't old enough.`);
+      }
+    }, 4000); 
+  });
+
+  checkEligibility
+    .then((message) => alert(message))
+    .catch((error) => alert(error));
+});
+
